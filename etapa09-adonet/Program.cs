dd1416c01ec6915ccb645c09868cc9fbab8fa279
Samuel Belo <samuel.belo@al.infnet.edu.br>
2020-02-28 09:06:57 -0300

    faz open insert e close no banco

--- etapa09-adonet/Program.cs
+++ etapa09-adonet/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace etapa09_adonet
 {
@@ -7,6 +9,32 @@ namespace etapa09_adonet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var input = Console.ReadLine();
+
+            var connectionString =
+                @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\samuel.belo\source\repos\etapa09-adonet\etapa09-adonet\AppData\mdfaula9.mdf;Integrated Security=True";
+            /// Alterar connection string para seu path
+
+            var cmdText = "INSERT INTO Pessoa" +
+                            "(Nome)" +
+                            "VALUES(@pessoaNome);";
+
+
+
+
+            using(var sqlConnection = new SqlConnection(connectionString))
+            using (var sqlCommand = new SqlCommand(cmdText, sqlConnection)) 
+            {
+                sqlCommand.CommandType = CommandType.Text;
+
+                sqlCommand.Parameters.Add("@pessoaNome", SqlDbType.VarChar).Value = input;
+
+                sqlConnection.Open();
+
+                var resultScalar = sqlCommand.ExecuteScalar();
+            }
+
+
         }
     }
 }
