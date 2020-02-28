b33cf7624e091015a3b682996cac0605965f0498
Samuel Belo <samuel.belo@al.infnet.edu.br>
2020-02-28 07:51:42 -0300

    Add project files.

---
+++ etapa09-adonet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace etapa09_adonet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
